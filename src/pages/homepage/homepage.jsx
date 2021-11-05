d8610105b3f099874ba644e9834b0b3e586d73ac
unsel <talha.unsel@plusclouds.com>
2021-11-05 19:33:57 -0400

    added react-select library, created the dummy searbar without functionality for now

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,12 +1,68 @@
-import React from "react";
 
+import React,{ useState , useEffect } from "react";
+import './homepage.scss';
+import Button from '@mui/material/Button';
+import Select from 'react-select'
+import {useNavigate} from 'react-router-dom';
 const HomePage = () => {
   
+    const [actionOption,setActionOption] = useState("buy")
+    const [currencyOption,setCurrencyOption] = useState("bitcoin")
 
+    const navigate = useNavigate();
+    const actionOptions = [
+        { value: 'buy', label: 'Buy' },
+        { value: 'sell', label: 'Sell' },
+    ]  
+    const currencyOptions = [
+        { value: 'bitcoin', label: 'Bitcoin' },
+        { value: 'ethereum', label: 'Ethereum' },
+        { value: 'doge', label: 'Doge' }
+    ]
+
+    const handleChange = (e,type) => {
+        if(type == "action"){
+            setActionOption(e.value)
+        } else {
+            setCurrencyOption(e.value)
+        }
+    }
     return(
-      <div>
-          This is homepage
-      </div>
+        <div className="homePage">
+            <div className="cryptoWatcher">
+                CRYPTOWATCHER
+            </div>
+            <div className="searchWrapper">
+                <div className="actionChoice">
+                    <Select 
+                        options={actionOptions}
+                        defaultValue={actionOptions[0]}
+                        label
+                        onChange={e => handleChange(e,"action")}
+                        name="Action"
+                        className="basic-multi-select"
+                        classNamePrefix="select" />
+                </div>
+                <div className="currencyChoice">
+                <Select 
+                        options={currencyOptions}
+                        defaultValue={currencyOptions[0]}
+                        label
+                        onChange={e => handleChange(e,"currency")}
+                        name="Currencies"
+                        className="basic-multi-select"
+                        classNamePrefix="select" />
+                </div>
+                <div className="searchButton">
+                <Button variant="contained" color="success" onClick={() => navigate('/result')}>
+                    Search
+                </Button>
+                </div>
+            </div>
+            {actionOption}
+            {currencyOption}
+        </div>
+      
     ) 
   }
   
