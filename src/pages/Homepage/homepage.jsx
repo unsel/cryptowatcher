835f5b8b2da1b8ed4dfc3c8dda4e126238b03252
unsel <talha.unsel@plusclouds.com>
2021-11-27 00:50:52 -0500

    changed homepage

--- src/pages/Homepage/homepage.jsx
+++ src/pages/Homepage/homepage.jsx
@@ -4,60 +4,22 @@ import './homepage.scss';
 import Button from '@mui/material/Button';
 import Select from 'react-select'
 import {useNavigate} from 'react-router-dom';
+import icon1 from '../../images/1.png'
 const HomePage = () => {
   
-    const [actionOption,setActionOption] = useState("buy")
-    const [currencyOption,setCurrencyOption] = useState("bitcoin")
-
-    const navigate = useNavigate();
-    const actionOptions = [
-        { value: 'buy', label: 'Buy' },
-        { value: 'sell', label: 'Sell' },
-    ]  
-    const currencyOptions = [
-        { value: 'bitcoin', label: 'Bitcoin' },
-        { value: 'ethereum', label: 'Ethereum' },
-        { value: 'doge', label: 'Doge' }
-    ]
-    const handleChange = (e,type) => {
-        if(type == "action"){
-            setActionOption(e.value)
-        } else {
-            setCurrencyOption(e.value)
-        }
-    }
+    
     return(
         <div className="homePage">
            
-            <div className="searchWrapper">
-                <div className="actionChoice">
-                    <Select 
-                        options={actionOptions}
-                        defaultValue={actionOptions[0]}
-                        label
-                        onChange={e => handleChange(e,"action")}
-                        name="Action"
-                        className="basic-multi-select"
-                        classNamePrefix="select" />
-                </div>
-                <div className="currencyChoice">
-                <Select 
-                        options={currencyOptions}
-                        defaultValue={currencyOptions[0]}
-                        label
-                        onChange={e => handleChange(e,"currency")}
-                        name="Currencies"
-                        className="basic-multi-select"
-                        classNamePrefix="select" />
-                </div>
-                <div className="searchButton">
-                <Button variant="contained" color="success" onClick={() => navigate('/result')}>
-                    Search
-                </Button>
-                </div>
-            </div>
-            {actionOption}
-            {currencyOption}
+           <div>
+               <div>
+                   <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"/>
+                   
+                   15000 DOLLARS
+                   <img src="https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/1.svg"/>
+                   </div>
+           </div>
+           
         </div>
       
     ) 
