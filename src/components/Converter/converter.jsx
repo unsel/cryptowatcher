7b1d27bbce28eb8aa4cb1e4909b5a5e245179d71
unsel <talha.unsel@plusclouds.com>
2021-11-28 12:47:27 -0500

    improved datagrid on homepage, improved converter and switched to static data for now

---
+++ src/components/Converter/converter.jsx
@@ -0,0 +1,109 @@
+import React,{useState,useEffect} from 'react';
+
+import './converter.scss';
+import TextField from '@mui/material/TextField';
+import Select from 'react-select'
+import { Button } from '@mui/material';
+import ThreeSixtyIcon from '@mui/icons-material/ThreeSixty';
+
+const Converter = (props) => {
+
+    const [leftOption,setLeftOption] = useState("USD")
+    const [rightOption,setRightOption] = useState("USD")
+    const [leftAmount,setLeftAmount] = useState()
+    const [rightAmount,setRightAmount] = useState()
+    const [values,setValues] = useState({'USD':1 })
+
+    useEffect(() => {
+        setValues(props.values)
+    }, [props.values]);
+
+    const currencyOptions = [
+        { value: 'USD', label: 'USD'},
+        { value: 'Bitcoin', label: 'Bitcoin' },
+        { value: 'Ethereum', label: 'Ethereum' },
+        { value: 'Litecoin', label: 'Litecoin'},
+        { value: 'Binance Coin', label: 'Binance Coin'},
+        { value: 'XRP', label: 'XRP' },
+        { value: 'Cardano', label: 'Cardano' },
+        { value: 'Dogecoin', label: 'Dogecoin' },
+        { value: 'Solana', label: 'Solana' },
+        { value: 'SHIBA INU', label: 'SHIBA INU'},
+        { value: 'Terracoin', label: 'Terracoin'},
+        { value: 'Avalanche', label: 'Avalanche' },
+        { value: 'Quark', label: 'Quark' }
+    ]  
+
+    const handleOptionChange = (e,type) => {
+        if(type === "left"){
+            setLeftOption(e.value)
+        } else {
+            setRightOption(e.value)
+        }
+    }
+    const handleAmountChange = (e,type) => {
+        if(type === "left"){
+            setLeftAmount(e.target.value)
+            setRightAmount(e.target.value*(values[leftOption]/values[rightOption]))
+            
+        } else {
+            setRightAmount(e.target.value)
+            setLeftAmount(e.target.value*(values[rightOption]/values[leftOption]))
+        }
+    }
+
+    return (
+     <div className="converter">
+        <div class="leftConverter">
+            <div className="currencyChoice">
+                <Select 
+                    options={currencyOptions}
+                    defaultValue={currencyOptions[0]}
+                    label
+                    onChange={e => handleOptionChange(e,"left")}
+                    name="Action"
+                    className="basic-multi-select"
+                    classNamePrefix="select" />
+            </div>
+            <TextField
+                id="outlined-number"
+                label="Number"
+                value={leftAmount}
+                placeholder="0"
+                onChange={(e)=>handleAmountChange(e,'left')}
+                type="number"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+            />
+        </div>
+        <ThreeSixtyIcon fontSize="large" className="threesixtyIcon"/>
+        <div class="rightConverter">
+            <div className="currencyChoice">
+                <Select 
+                    options={currencyOptions}
+                    defaultValue={currencyOptions[0]}
+                    label
+                    onChange={e => handleOptionChange(e,"right")}
+                    name="Action"
+                    className="basic-multi-select"
+                    classNamePrefix="select" />
+            </div>
+            <TextField
+            id="outlined-number"
+            label="Number"
+            value={rightAmount}
+            placeholder="0"
+            type="number"
+            InputLabelProps={{
+                shrink: true,
+            }}
+            onChange={(e)=>handleAmountChange(e,'right')}
+            />
+        </div>
+    
+    
+     </div>
+    );
+}
+export default Converter;
--- src/components/CryptoTable/cryptotable.jsx
+++ src/components/CryptoTable/cryptotable.jsx
@@ -10,11 +10,12 @@ const DataTable = (props) => {
         { field: 'id', headerName: 'ID', width: 60 },
         { field: 'logo',headerName: 'Logo',width: 70, editable: true, renderCell: (params) => <img alt="currenyLogo" className="currencyLogo" src={params.value} />},
         { field: 'name', headerName: 'Name', width: 100 },
-        { field: 'price', headerName: 'Price ($)', width: 130 },
-        { field: 'percent_change_24h', headerName: '24h%', width: 90 },
-        { field: 'market_cap', headerName: 'Market Cap ($)', width: 180 },
-        { field: 'volume_24h', headerName: 'Volume(24h)($)', width: 180 },
-        { field: 'circulating_supply', headerName: 'Circulating Supply', width: 150 },
+        { field: 'price', headerName: 'Price ($)', width: 120 },
+        { field: 'percent_change_24h', headerName: 'Daily Change %', width: 130 },
+        { field: 'percent_change_30d', headerName: 'Monthly Change %', width: 150 },
+        { field: 'market_cap', headerName: 'Market Cap ($)', width: 200 },
+        { field: 'volume_24h', headerName: 'Daily Volume($)', width: 160 },
+        { field: 'volume_change_24h', headerName: 'Daily Change($)', width: 130 },
         { field: 'graph',headerName: 'Last 7 Days',width: 190,editable: true,renderCell: (params) => <img alt="currenyGraph" src={params.value} />},
     ];
     //   RETURNED DATA FOR NOW
@@ -33,7 +34,7 @@ const DataTable = (props) => {
    
     useEffect(() => {
       setFetching(true)
-      const ourKeys = ['name', 'price','percent_change_24h','market_cap','volume_24h','circulating_supply','logo','graph']
+      const ourKeys = ['name', 'price','percent_change_24h','percent_change_30d','market_cap','volume_24h','volume_change_24h','logo','graph']
       if(Object.keys(props.currencyData).length !== 0) {
         let currencyDataList=[]
         let id=1
@@ -44,7 +45,16 @@ const DataTable = (props) => {
           for (const [key,value] of Object.entries(currData)) {
             temp['name'] = currName
             if(ourKeys.includes(key)){
-              temp[key] = value
+              if(['id','logo','name','graph'].includes(key)){
+                temp[key] = value
+              } else {
+                temp[key] =value.toFixed(3).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+              }
+
+              if (['price','market_cap','volume_24h'].includes(key)){
+                temp[key] ='$' + temp[key]
+              }
+              
             }
           }
           currencyDataList.push(temp)
@@ -53,18 +63,16 @@ const DataTable = (props) => {
        Promise.resolve(setRows(currencyDataList)).then(()=>setFetching(false))
       }
       
-        // setRows([{ id: 1, name: 'Bitcoin', price:57569,dailyChange: +1.47,marketCap:"1,038,645,607,815",dailyVolume:"31,315,223,996",circulatingSupply:"18,884,712", logo:"https://s2.coinmarketcap.com/static/img/coins/64x64/1.png",graph:"https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/1.svg"},
-        //         { id: 2, name: 'Ethereum', price:58000,dailyChange: -1.47,marketCap:"1,038,645,607,812",dailyVolume:"42,353,535,535",circulatingSupply:"17,333", logo:"https://s2.coinmarketcap.com/static/img/coins/64x64/1.png",graph:"https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/1.svg"}])
     }, [props.currencyData]);
 
   return (
-      <div style={{ height: 750, width: '100%' }}>
+      <div style={{ height: 850, width: '100%' }}>
         <DataGrid
           loading={fetching}
           rows={rows}
           columns={columns}
           rowHeight={90}
-          pageSize={5}
+          pageSize={15}
           rowsPerPageOptions={[5]}
           checkboxSelection
         />
